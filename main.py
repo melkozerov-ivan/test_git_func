2bfe845636309a617b411138895e5aac53240349
melkozerov-ivan <118188510+melkozerov-ivan@users.noreply.github.com>
2022-12-12 18:34:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-if __name__ == '__main__':
-    with open('.env') as f:
-        password = f.read()
+def main():
+    if __name__ == '__main__':
+        with open('.env') as f:
+            password = f.read()
 
-    print(password)
+        print(password)
